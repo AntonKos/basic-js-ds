e41d381d6e5a7bb12438f0af7a5392b63b097490
Anton <antonkosicyn@rambler.ru>
2022-05-02 14:24:23 +0300

    feat:smth

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -7,26 +7,65 @@ const { NotImplementedError } = require('../extensions/index.js');
 * using Node from extensions
 */
 class BinarySearchTree {
+  constructor() {
+    this.root_1 = null
+    this.value = null
+    this.isHave = false
+}
 
   root() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.root_1
   }
 
-  add(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+  add(data) {
+    const newNode = new Node(data)
+    if (!this.root_1) {
+        this.root_1 = newNode
+        return
+    }
+    let currentNode = this.root_1
 
-  has(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    while (currentNode) {
+        if (newNode.data < currentNode.data) {
+            if (!currentNode.left) {
+                currentNode.left = newNode
+                return
+            }
 
-  find(/* data */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+            currentNode = currentNode.left
+        } else {
+            if (!currentNode.right) {
+                currentNode.right = newNode
+                return
+            }
+
+            currentNode = currentNode.right
+        }
+    }
   }
+  find(data) {
+
+    this.data = data
+
+    this.preOrder(this.root_1, (node) => {
+        if (node.data === this.data) {
+            this.isHave = true
+            this.findedObj = node
+        }
+    })
+    if (this.isHave) {
+        this.isHave = false
+        return this.findedObj
+    } else {
+        return null
+    }
+
+}
+  
+
+  // find( data ) {
+
+  // }
 
   remove(/* data */) {
     throw new NotImplementedError('Not implemented');
